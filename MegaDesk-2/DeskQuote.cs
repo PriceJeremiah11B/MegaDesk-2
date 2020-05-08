0446126d7c6dfb0cbbd865c48e49822ac04b3ea2
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2020-05-08 11:46:27 -0600

    Add project files.

---
+++ MegaDesk-2/DeskQuote.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_3_Jason_Chantry
+{
+    class DeskQuote
+    {
+        public string customer { get; set; }
+        public List<Desk> desks
+        {
+            get; private set;
+        }
+        public DeskQuote()
+        {
+            desks = new List<Desk>();
+        }
+        public float total
+        {
+            get
+            {
+                float total = (float)0.0;
+                foreach (var desk in desks)
+                {
+                    total += desk.price;
+                }
+                return total;
+            }
+        }
+        public void addDesk(Desk desk)
+        {
+            this.desks.Add(desk);
+        }
+        public void removeDesk(int index)
+        {
+            this.desks.RemoveAt(index);
+        }
+        public void removeDesk(Desk desk)
+        {
+            this.desks.Remove(desk);
+        }
+    }
+}
---
+++ MegaDesk-2/MainMenu.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MegaDesk_2
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.createQuote = new System.Windows.Forms.Button();
+            this.displayQuote = new System.Windows.Forms.Button();
+            this.viewQuote = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // createQuote
+            // 
+            this.createQuote.Font = new System.Drawing.Font("Noto Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createQuote.Location = new System.Drawing.Point(29, 164);
+            this.createQuote.Name = "createQuote";
+            this.createQuote.Size = new System.Drawing.Size(183, 50);
+            this.createQuote.TabIndex = 0;
+            this.createQuote.Text = "Create Quote";
+            this.createQuote.UseVisualStyleBackColor = true;
+            this.createQuote.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayQuote
+            // 
+            this.displayQuote.Font = new System.Drawing.Font("Noto Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayQuote.Location = new System.Drawing.Point(29, 235);
+            this.displayQuote.Name = "displayQuote";
+            this.displayQuote.Size = new System.Drawing.Size(183, 50);
+            this.displayQuote.TabIndex = 1;
+            this.displayQuote.Text = "Display Quote";
+            this.displayQuote.UseVisualStyleBackColor = true;
+            // 
+            // viewQuote
+            // 
+            this.viewQuote.Font = new System.Drawing.Font("Noto Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewQuote.Location = new System.Drawing.Point(29, 312);
+            this.viewQuote.Name = "viewQuote";
+            this.viewQuote.Size = new System.Drawing.Size(183, 50);
+            this.viewQuote.TabIndex = 2;
+            this.viewQuote.Text = "View All Quotes";
+            this.viewQuote.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Noto Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(29, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Search Quotes";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Noto Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(605, 388);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(353, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(398, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 33);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Welcome to Mega Desk";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.viewQuote);
+            this.Controls.Add(this.displayQuote);
+            this.Controls.Add(this.createQuote);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainMenu";
+            this.Text = "Mega Desk Imporium";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createQuote;
+        private System.Windows.Forms.Button displayQuote;
+        private System.Windows.Forms.Button viewQuote;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
